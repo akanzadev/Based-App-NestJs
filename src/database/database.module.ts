7bf23b96b8dc15220d567fa3165ec0960c47a211
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-06-28 16:37:20 -0500

    ssl

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,34 +11,22 @@ import { User, Role, RoleToUser } from './entities/users';
     TypeOrmModule.forRootAsync({
       inject: [config.KEY],
       useFactory: async (configService: ConfigType<typeof config>) => {
-        const { dbName, host, password, port, user, url } = configService.mysql;
-        const { nodeEnv } = configService.scope;
-        // Connect with DATABASE_URL env variable
-        let config = {};
-        if (nodeEnv === 'production') {
-          config = {
-            type: 'mysql',
-            url,
-            synchronize: false,
-            logging: false,
-            autoLoadEntities: true,
-            ssl: { rejectUnauthorized: false },
-          };
-        } else {
-          config = {
-            type: 'mysql',
-            host,
-            port,
-            username: user,
-            password,
-            database: dbName,
-            synchronize: false,
-            logging: false,
-            autoLoadEntities: true,
-            ssl: false,
-          };
-        }
-        return config;
+        const {
+          mysql: { dbName, host, password, port, user },
+          scope: { nodeEnv },
+        } = configService;
+        return {
+          type: 'mysql',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+          synchronize: false,
+          logging: false,
+          autoLoadEntities: true,
+          ssl: nodeEnv === 'production' ? { rejectUnauthorized: false } : false,
+        };
       },
     }),
     TypeOrmModule.forFeature([User, Role, RoleToUser]),
